749a050ebabc364c47fb7ea19cb3c94b84c6c78e
esindelabi <esindelabi@gmail.com>
2025-10-09 20:34:11 +0100

    Make changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ const Header: React.FC = () => {
     return (
         <header className="header-section">
             <div className="header-content">
-                <h1 className="header-title">Modupe Sounton</h1>
+                <h1 className="header-title">Modupe SOUNTON</h1>
                 <h3 className="header-subtitle">Fullstack Developper</h3>
                 <p className="header-description">
                     Donnez vie à des expériences web uniques.
